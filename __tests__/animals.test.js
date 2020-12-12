ab8cf28d2dfa78c1d63febc98808796eddbeb02b
davidabutler92 <davidabutler92@yahoo.com>
2020-12-12 00:02:52 -0800

    add passing test for POST route

--- __tests__/animals.test.js
+++ __tests__/animals.test.js
@@ -2,6 +2,7 @@ const app = require('../lib/app');
 const fs = require('fs');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
+const Animal = require('../lib/models/Animal');
 
 describe('animals routes', () => {
   beforeEach(() => {
@@ -21,9 +22,24 @@ describe('animals routes', () => {
       });
 
     expect(res.body).toEqual({
+      id: '1',
       color: 'red',
       type: 'mammal'
     });
   }); 
   
+it('shoud get all animals', async() => {
+    const animals = await Promise.all([
+      { color: 'brown', type: 'bear' },
+      { color: 'blue', type: 'whale' },
+      { color: 'red', type: 'snake' }
+    ].map(animal => Animal.insert(animal)));
+
+    const res = await request(app)
+      .get('/api/v1/animals');
+    
+    expect(res.body).toEqual(expect.arrayContaining(animals));
+    expect(res.body).toHaveLength(animals.length);
+});
+
 });
