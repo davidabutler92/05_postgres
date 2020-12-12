84d4765aada90f51de0a74ea7541725078da319f
davidabutler92 <davidabutler92@yahoo.com>
2020-12-11 21:51:25 -0800

    add test commit

--- __tests__/animals.test.js
+++ __tests__/animals.test.js
@@ -3,3 +3,27 @@ const fs = require('fs');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
 
+describe('animals routes', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('should create a new animal using POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/animals')
+      .send({
+        color: 'red',
+        type: 'mammal'
+      });
+
+    expect(res.body).toEqual({
+      color: 'red',
+      type: 'mammal'
+    });
+  }); 
+  
+});
